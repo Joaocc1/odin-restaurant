1008949b47eb85fdd9689b7b9135de46be5bcaff
Joao Colaco <114249810+Joaocc1@users.noreply.github.com>
2026-06-28 16:50:17 +0100

    add and remove class that shows which section the user is on

--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ import Menu from "./pages/Menu.js";
 import Contact from "./pages/Contact.js";
 
 const content = document.querySelector("#content");
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const contactBtn = document.querySelector("#contact-btn");
 
-// Home(content);
+homeBtn.addEventListener("click", () => {
+  content.replaceChildren();
+  homeBtn.classList.add("btn-selected");
+  menuBtn.classList.remove("btn-selected");
+  contactBtn.classList.remove("btn-selected");
+  Home(content);
+});
+
+menuBtn.addEventListener("click", () => {
+  content.replaceChildren();
+  menuBtn.classList.add("btn-selected");
+  homeBtn.classList.remove("btn-selected");
+  contactBtn.classList.remove("btn-selected");
+  Menu(content);
+});
+
+contactBtn.addEventListener("click", () => {
+  content.replaceChildren();
+  contactBtn.classList.add("btn-selected");
+  menuBtn.classList.remove("btn-selected");
+  homeBtn.classList.remove("btn-selected");
+  Contact(content);
+});
+
+Home(content);
 // Menu(content);
 // Contact(content);
